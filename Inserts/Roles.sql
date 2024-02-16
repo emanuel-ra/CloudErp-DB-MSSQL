f0e273591b34cc17f6d953a71a1007062979f5b9
Emanuel Ramirez Abarca <85582567+emanuel-ra@users.noreply.github.com>
2024-02-16 12:17:47 -0600

    ➕ #ADD: Insert Role

---
+++ Inserts/Roles.sql
@@ -0,0 +1,5 @@
+/* * Cat Status table should be created and have data */
+/* * BY DEFAULT THE ACTIVE STATUS SHOULD BE EQUAL TO 1 */
+SET IDENTITY_INSERT CatStatus ON
+INSERT INTO Roles(Id,Name,StatusId) VALUES (1,'Developer',1)
+SET IDENTITY_INSERT CatStatus OFF
