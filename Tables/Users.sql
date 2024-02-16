efe4707bc10b1a938b8fb052fbd2b7d8b9afda2d
Emanuel Ramirez Abarca <85582567+emanuel-ra@users.noreply.github.com>
2024-02-16 12:07:55 -0600

    ➕ ADD: Create Table Users

---
+++ Tables/Users.sql
@@ -0,0 +1,12 @@
+CREATE TABLE Users(
+    Id INT PRIMARY KEY IDENTITY(1,1) , 
+    Name VARCHAR(250) , 
+    UserName VARCHAR(250) , 
+    ProfilePhoto VARCHAR(250) , 
+    Email VARCHAR(250) , 
+    Password VARCHAR(250) , 
+    StatusId INT NOT NULL , 
+    RoleId INT NOT NULL , 
+    CreatedAt DATETIME NOT NULL DEFAULT GETDATE() , 
+    UpdatedAt DATETIME NOT NULL DEFAULT GETDATE() , 
+)
