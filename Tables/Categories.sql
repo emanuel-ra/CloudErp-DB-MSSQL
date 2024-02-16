2d8cbadbdca2d7a26967c559df26967f999b2c1d
Emanuel Ramirez Abarca <85582567+emanuel-ra@users.noreply.github.com>
2024-02-16 12:39:24 -0600

    ➕ #ADD: Create Table Categories

---
+++ Tables/Categories.sql
@@ -0,0 +1,12 @@
+CREATE TABLE Categories(
+    Id INT PRIMARY KEY IDENTITY(1,1) , 
+    ParentId INT NOT NULL , 
+    Name VARCHAR(250) , 
+    Logo VARCHAR(250) , 
+    IsEnableECommerce INT NOT NULL DEFAULT 0 
+    StatusId INT NOT NULL ,
+    UserId INT NOT NULL ,
+    ExternalId INT NOT NULL DEFAULT 0 ,
+    CreatedAt DATETIME NOT NULL DEFAULT GETDATE() , 
+    UpdatedAt DATETIME NOT NULL DEFAULT GETDATE() 
+)
