f3c65c2068077e291672e77f6de5a5e5d3651d56
Emanuel Ramirez Abarca <85582567+emanuel-ra@users.noreply.github.com>
2024-02-16 12:33:04 -0600

    ➕ #ADD: Create Table Brands

---
+++ Tables/Brands.sql
@@ -0,0 +1,11 @@
+CREATE TABLE Brands(
+    Id INT PRIMARY KEY IDENTITY(1,1) , 
+    Name VARCHAR(250) , 
+    Logo VARCHAR(250) , 
+    IsEnableECommerce INT NOT NULL DEFAULT 0 
+    StatusId INT NOT NULL ,
+    UserId INT NOT NULL , 
+    ExternalId INT NOT NULL DEFAULT 0 ,
+    CreatedAt DATETIME NOT NULL DEFAULT GETDATE() , 
+    UpdatedAt DATETIME NOT NULL DEFAULT GETDATE() , 
+)
