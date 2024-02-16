96aa6fc0c6324a6a788e8f0ce4cb48de9b7c7231
Emanuel Ramirez Abarca <85582567+emanuel-ra@users.noreply.github.com>
2024-02-16 13:27:13 -0600

    ➕ #ADD: Create Table Relation Category to Product

---
+++ Tables/RelCategoriesToProducts.sql
@@ -0,0 +1,8 @@
+CREATE TABLE RelCategoriesToProducts(
+    Id INT PRIMARY KEY IDENTITY(1,1) , 
+    CategoryId  INT NOT NULL , 
+    ProductId  INT NOT NULL , 
+    UserId INT NOT NULL , 
+    CreatedAt DATETIME NOT NULL DEFAULT GETDATE() , 
+    UpdatedAt DATETIME NOT NULL DEFAULT GETDATE() 
+)
