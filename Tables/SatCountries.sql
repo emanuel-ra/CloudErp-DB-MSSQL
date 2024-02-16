6c0aafa9233d4afd525b6a6615106f4b7a557f52
Emanuel Ramirez Abarca <85582567+emanuel-ra@users.noreply.github.com>
2024-02-16 13:54:49 -0600

    ➕ #ADD: Create Table SatCountries

---
+++ Tables/SatCountries.sql
@@ -0,0 +1,8 @@
+CREATE TABLE SatCountries(
+    Id INT PRIMARY KEY IDENTITY(1,1) ,
+    Code VARCHAR(50) , 
+    Name VARCHAR(250) , 
+    StatusId INT NOT NULL ,
+    CreatedAt DATETIME NOT NULL DEFAULT GETDATE() , 
+    UpdatedAt DATETIME NOT NULL DEFAULT GETDATE() 
+)
