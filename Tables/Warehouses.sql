d3fd5f7453b2e6ae9d312ed4b9e49db332bd8915
Emanuel Ramirez Abarca <85582567+emanuel-ra@users.noreply.github.com>
2024-02-17 13:36:48 -0600

    🖊 #EDIT: Edit File Warehouses: Rename columns

--- Tables/Warehouses.sql
+++ Tables/Warehouses.sql
@@ -1,14 +1,14 @@
 CREATE TABLE Warehouses(
-    ID INT PRIMARY KEY IDENTITY(1,1) , 
-    code VARCHAR(100) , 
-    name VARCHAR(250) , 
-    branch_id INT NOT NULL , 
-    address VARCHAR(250) , 
-    contact VARCHAR(150) , 
-    tel VARCHAR(100) , 
-    is_locked INT NOT NULL DEFAULT 0 , 
-    enable_to_download_stock INT NOT NULL DEFAULT 0 ,
-    download_at DATETIME NOT NULL DEFAULT GETDATE() , 
-    external_id INT NOT NULL DEFAULT 0 , 
-    idStatus INT NOT NULL DEFAULT 1 
+    id INT PRIMARY KEY IDENTITY(1,1) , 
+    Code VARCHAR(100) , 
+    Name VARCHAR(250) , 
+    BranchId INT NOT NULL , 
+    Address VARCHAR(250) , 
+    Contact VARCHAR(150) , 
+    Telephone VARCHAR(100) , 
+    IsLocked INT NOT NULL DEFAULT 0 , 
+    EnableToDownloadStock INT NOT NULL DEFAULT 0 ,
+    DownloadAt DATETIME NOT NULL DEFAULT GETDATE() , 
+    ExternalId INT NOT NULL DEFAULT 0 , 
+    StatusId INT NOT NULL DEFAULT 1 
 )
