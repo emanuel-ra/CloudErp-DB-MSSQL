50f265fffa10a5694441427e403df2b8bac23ac1
Emanuel Ramirez Abarca <85582567+emanuel-ra@users.noreply.github.com>
2024-02-17 13:32:55 -0600

    ➕ #ADD: Column [Version] on [SatZipCodes]

--- Tables/SatZipCodes.sql
+++ Tables/SatZipCodes.sql
@@ -1,9 +1,10 @@
 CREATE TABLE SatZipCodes(
     Id INT PRIMARY KEY IDENTITY(1,1) ,
     Code VARCHAR(50) , 
+    Version VARCHAR(10) , 
     StateCode VARCHAR(50) , 
     CountryCode VARCHAR(50) , 
-    LocalityCode VARCHAR(50) , 
+    LocalityCode VARCHAR(50) ,     
     StatusId INT NOT NULL ,
     CreatedAt DATETIME NOT NULL DEFAULT GETDATE() , 
     UpdatedAt DATETIME NOT NULL DEFAULT GETDATE() 
