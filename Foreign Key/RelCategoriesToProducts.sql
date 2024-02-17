fee9f564dad42e817608344c47694d91b7d6396f
Emanuel Ramirez Abarca <85582567+emanuel-ra@users.noreply.github.com>
2024-02-17 13:11:50 -0600

    🔑 #ADD: File Foreign key on  RelCategoriesToProducts Table

---
+++ Foreign Key/RelCategoriesToProducts.sql
@@ -0,0 +1,5 @@
+ALTER TABLE RelCategoriesToProducts ADD FOREIGN KEY (CategoryId) REFERENCES Categories(Id)
+
+ALTER TABLE RelCategoriesToProducts ADD FOREIGN KEY (ProductId) REFERENCES Products(Id)
+
+ALTER TABLE RelCategoriesToProducts ADD FOREIGN KEY (UserId) REFERENCES (Id)
