d16e4b6151d8a663251825b7261eded6ccbd235f
Emanuel Ramirez Abarca <85582567+emanuel-ra@users.noreply.github.com>
2024-04-19 18:29:46 -0600

    add list of prices and remove prices from products

---
+++ Inserts/ProductsListPrice.sql
@@ -0,0 +1,20 @@
+INSERT INTO ProductListPrice (Name,Currency,CreatedAt,UpdatedAt)
+	VALUES ('Market Price','MXN',GETDATE(),GETDATE())
+
+INSERT INTO ProductListPrice (Name,Currency,CreatedAt,UpdatedAt)
+	VALUES ('Wholesale Price','MXN',GETDATE(),GETDATE())
+
+INSERT INTO ProductListPrice (Name,Currency,CreatedAt,UpdatedAt)
+	VALUES ('Member Price','MXN',GETDATE(),GETDATE())
+
+INSERT INTO ProductListPrice (Name,Currency,CreatedAt,UpdatedAt)
+	VALUES ('Box Price','MXN',GETDATE(),GETDATE())
+
+INSERT INTO ProductListPrice (Name,Currency,CreatedAt,UpdatedAt)
+	VALUES ('Vip Price','MXN',GETDATE(),GETDATE())
+
+INSERT INTO ProductListPrice (Name,Currency,CreatedAt,UpdatedAt)
+	VALUES ('Premium Price','MXN',GETDATE(),GETDATE())
+
+INSERT INTO ProductListPrice (Name,Currency,CreatedAt,UpdatedAt)
+	VALUES ('Plus Price','MXN',GETDATE(),GETDATE())
