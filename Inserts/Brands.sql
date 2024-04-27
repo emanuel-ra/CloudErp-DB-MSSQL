2cfafb3824030a1515d480c45804c88e8d18a7be
Emanuel Ramirez Abarca <85582567+emanuel-ra@users.noreply.github.com>
2024-04-26 18:11:47 -0600

    Fix: adapt to mariadb

--- Inserts/Brands.sql
+++ Inserts/Brands.sql
@@ -1,13 +1,12 @@
-SET IDENTITY_INSERT Brands ON
+
 INSERT INTO Brands (Id, Name ,Logo ,IsEnableECommerce ,StatusId ,UserId ,ExternalId ,CreatedAt ,UpdatedAt)
-    VALUES (1, 'Indefinido' ,'' ,0 ,1 ,1 ,0 ,GETDATE() ,GETDATE())
+    VALUES (1, 'Indefinido' ,'' ,0 ,1 ,1 ,0 ,NOW() ,NOW());
     
 INSERT INTO Brands (Id, Name ,Logo ,IsEnableECommerce ,StatusId ,UserId ,ExternalId ,CreatedAt ,UpdatedAt)
-    VALUES (2, 'LinkBits' ,'' ,1 ,1 ,1 ,0 ,GETDATE() ,GETDATE())
+    VALUES (2, 'LinkBits' ,'' ,1 ,1 ,1 ,0 ,NOW() ,NOW());
 
 INSERT INTO Brands (Id, Name ,Logo ,IsEnableECommerce ,StatusId ,UserId ,ExternalId ,CreatedAt ,UpdatedAt)
-    VALUES (3, 'SanDisk' ,'' ,1 ,1 ,1 ,0 ,GETDATE() ,GETDATE())
+    VALUES (3, 'SanDisk' ,'' ,1 ,1 ,1 ,0 ,NOW() ,NOW());
 
 INSERT INTO Brands (Id, Name ,Logo ,IsEnableECommerce ,StatusId ,UserId ,ExternalId ,CreatedAt ,UpdatedAt)
-    VALUES (4, 'Vamav' ,'' ,1 ,1 ,1 ,0 ,GETDATE() ,GETDATE())
-SET IDENTITY_INSERT Brands OFF
+    VALUES (4, 'Vamav' ,'' ,1 ,1 ,1 ,0 ,NOW() ,NOW());
