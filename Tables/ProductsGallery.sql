e75ff71b45afc1b370c7c6acbe68d2a877e8c089
Emanuel Ramirez Abarca <85582567+emanuel-ra@users.noreply.github.com>
2024-06-20 10:43:37 -0600

    feat(table): add ProductGallery table

--- Tables/ProductsGallery.sql
+++ Tables/ProductsGallery.sql
@@ -1,8 +1,14 @@
 CREATE TABLE ProductsGallery(
     Id  INT PRIMARY KEY IDENTITY(1,1) , 
-    ProductId INT NOT NULL , 
-    ImageUrl VARCHAR(500) ,
+    ProductId INT NOT NULL ,     
+    Width int , 
+    Height int , 
+    Version int , 
+    Format VARCHAR(10) ,    
+    PublicId VARCHAR(200) ,    
+    Url VARCHAR(500) ,
+    SecureUrl VARCHAR(500) ,        
     UserId INT NOT NULL , 
-    CreatedAt DATETIME NOT NULL DEFAULT GETDATE() , 
-    UpdatedAt DATETIME NOT NULL DEFAULT GETDATE()
+    CreatedAt DATETIME NOT NULL DEFAULT GETDATE()  , 
+    UpdatedAt DATETIME NOT NULL DEFAULT GETDATE() 
 )
