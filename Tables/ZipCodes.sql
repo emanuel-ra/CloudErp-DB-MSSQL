2ea1cc9d8317f324882f5a71fb422aae5e6d52f9
Emanuel Ramirez Abarca <85582567+emanuel-ra@users.noreply.github.com>
2024-06-20 10:32:12 -0600

    feat(table): add zip codes table

---
+++ Tables/ZipCodes.sql
@@ -0,0 +1,8 @@
+CREATE TABLE ZipCodes(
+    ID INT PRIMARY KEY IDENTITY(1,1) , 
+    Zipcode VARCHAR(250) , 
+    State VARCHAR(250) , 
+    Town VARCHAR(250) , 
+    Country VARCHAR(250) , 
+    District VARCHAR(250) 
+)
