a9a0113df6b208cfad143468e8aa6602f3e86ee7
Emanuel Ramirez Abarca <85582567+emanuel-ra@users.noreply.github.com>
2024-07-06 12:56:22 -0600

    edit and change name Routes

--- Tables/Routes.sql
+++ Tables/Routes.sql
@@ -1,9 +1,8 @@
-CREATE TABLE FrontendRoutes(
-    Id  INT PRIMARY KEY IDENTITY(1,1) , 
+CREATE TABLE Routes(
+    Id  INT PRIMARY KEY IDENTITY(1,1) ,     
     ParentId INT ,
+    Code VARCHAR(250) UNIQUE , 
     Title VARCHAR(250) , 
-    Path VARCHAR(250) , 
-    Element VARCHAR(150) , 
     isEnable int not null default 0 , 
     CreatedAt DATETIME NOT NULL DEFAULT GETDATE() , 
     UpdatedAt DATETIME NOT NULL DEFAULT GETDATE() 
