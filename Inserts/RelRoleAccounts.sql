4b716b352de4b6a482b9971ebeac5f7d5667e8cb
Emanuel Ramirez Abarca <85582567+emanuel-ra@users.noreply.github.com>
2024-09-05 10:30:57 -0600

    add insert of rel accounts

---
+++ Inserts/RelRoleAccounts.sql
@@ -0,0 +1,4 @@
+INSERT INTO RelRoleAccounts(RoleId,AccountId,IsDefault, CreatedAt, UpdatedAt )
+	VALUES (1,1,1, GETDATE(), GETDATE()) ,
+		(1,2,0, GETDATE(), GETDATE()) ,
+		(1,3,0, GETDATE(), GETDATE()) 
