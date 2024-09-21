d2d8ba53481484e9eb2d06e43997f4d2c57cfed2
Emanuel Ramirez Abarca <85582567+emanuel-ra@users.noreply.github.com>
2024-09-21 14:12:18 -0600

    add accountid

--- Tables/ConfigurationSettings.sql
+++ Tables/ConfigurationSettings.sql
@@ -1,5 +1,6 @@
 CREATE TABLE ConfigurationSettings (
     Id INT PRIMARY KEY IDENTITY(1,1),
+    AccountId INT NOT NULL , 
     KeyId INT NOT NULL,
     Value VARCHAR(MAX) NOT NULL,
     FOREIGN KEY (KeyId) REFERENCES ConfigurationKeys(Id),
